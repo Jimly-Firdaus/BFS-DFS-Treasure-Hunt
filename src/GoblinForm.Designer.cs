ce274a36c1fa8ce9bcfe6182d484a03bac1c0468
Jeffrey Chow <13521046@std.stei.itb.ac.id>
2023-03-19 01:34:18 +0700

    feat: basic UI, readFile, coloringRoute

---
+++ src/GoblinForm.Designer.cs
@@ -0,0 +1,117 @@
+﻿using System.Diagnostics;
+
+namespace Goblin
+{
+    partial class GoblinForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel_input = new Panel();
+            panel_output = new Panel();
+            panel_title = new Panel();
+            SuspendLayout();
+
+            // Form dimensions
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(940, 414);
+
+            formWidth = ClientRectangle.Size.Width;
+            formHeight = ClientRectangle.Size.Height;
+            Debug.WriteLine(formWidth);
+            Debug.WriteLine(formHeight);    
+
+            // 
+            // panel_input
+            // 
+            panel_input.BackColor = Color.Yellow;
+            panel_input.Location = new Point((int)(0.05 * formWidth), (int)(0.2 * formHeight));
+            panel_input.Margin = new Padding(2, 1, 2, 1);
+            panel_input.Name = "panel_input";
+            panel_input.Size = new Size((int)(0.225 * formWidth),(int)(0.75 * formHeight));
+            panel_input.TabIndex = 0;
+            // 
+            // panel_output
+            // 
+            panel_output.BackColor = Color.Yellow;
+            panel_output.Location = new Point((int)(0.275 * formWidth), (int)(0.2 * formHeight));
+            panel_output.Margin = new Padding(2, 1, 2, 1);
+            panel_output.Name = "panel_output";
+            panel_output.Size = new Size((int)(0.675 * formWidth), (int)(0.75 * formHeight));
+            panel_output.TabIndex = 1;
+            // 
+            // panel_title
+            // 
+            panel_title.BackColor = Color.Yellow;
+            panel_title.Location = new Point((int)(0.05 * formWidth), (int)(0.05 * formHeight));
+            panel_title.Name = "panel_title";
+            panel_title.Size = new Size((int)(0.9 * formWidth), (int)(0.1 * formHeight));
+            panel_title.TabIndex = 2;
+            // 
+            // GoblinForm
+            // 
+            Controls.Add(panel_title);
+            Controls.Add(panel_output);
+            Controls.Add(panel_input);
+            Margin = new Padding(2, 1, 2, 1);
+            Name = "GoblinForm";
+            Text = "Goblin";
+            ResumeLayout(false);
+
+            // Onchange
+            this.SizeChanged += new EventHandler(GoblinForm_Resize);
+        }
+
+        private void GoblinForm_Resize(object sender, EventArgs e)
+        {
+            // Update the client size to match the new size of the client area
+            ClientSize = new Size(ClientRectangle.Width, ClientRectangle.Height);
+
+            // Update form size
+            formWidth = ClientRectangle.Size.Width;
+            formHeight = ClientRectangle.Size.Height;
+
+            // update all panel
+            panel_input.Location = new Point((int)(0.05 * formWidth), (int)(0.2 * formHeight));
+            panel_input.Size = new Size((int)(0.225 * formWidth), (int)(0.75 * formHeight));
+
+            panel_output.Location = new Point((int)(0.275 * formWidth), (int)(0.2 * formHeight));
+            panel_output.Size = new Size((int)(0.675 * formWidth), (int)(0.75 * formHeight));
+
+            panel_title.Location = new Point((int)(0.05 * formWidth), (int)(0.05 * formHeight));
+            panel_title.Size = new Size((int)(0.9 * formWidth), (int)(0.1 * formHeight));
+        }
+
+        #endregion
+
+        private int formWidth;
+        private int formHeight;
+        internal Panel panel_input;
+        internal Panel panel_output;
+        private Panel panel_title;
+    }
+}
---
+++ src/GoblinForm.cs
@@ -0,0 +1,182 @@
+using System.Drawing;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Goblin
+{
+    public partial class GoblinForm : Form
+    {
+        private Panel[,] _panels;
+        private char[,] _maze;
+        private Point krustyKrab;
+
+        public GoblinForm()
+        {
+            InitializeComponent();
+            // filepath will be get from filedialogbox
+            readFile("C:\\Users\\Jeffrey Chow\\Documents\\ITB\\4th Semester\\IF2211 Algorithm Strategies\\Tubes 2\\test\\TestingApp\\TestingApp\\test.txt");
+            handleInputPanel();
+            handleOutputPanel();
+
+            char[] routes = { 'R', 'D', 'D', 'R', 'R', 'U' };
+            updateColorFromRoute(routes);
+        }
+
+        private void readFile(string filename)
+        {
+            // read file from text file
+            string[] lines = File.ReadAllLines(filename);
+
+            // replace " " to "" for each line in lines
+            for (int i = 0; i < lines.Length; i++)
+            {
+                lines[i] = lines[i].Replace(" ", "");
+            }
+            
+            // count rows and columns
+            int rows = lines.Length;
+            int columns = lines[0].Length;
+           
+            // initialize _panels and _maze
+            _panels = new Panel[rows, columns];
+            _maze = new char[rows, columns];
+
+            // fill the mazePanel
+            for (int i = 0; i < rows; i++)
+            {
+                char[] row = lines[i].ToCharArray();
+                for (int j = 0; j < columns; j++) {
+                    createNewPanel(i, j, row[j]);
+                    _maze[i, j] = row[j];   
+                }
+            }
+        }
+
+        private void createNewPanel(int i, int j, char type)
+        {
+            // construct panel
+            Panel panel = new Panel();
+
+            // add event handler for panel_output's SizeChanged event
+            panel_output.SizeChanged += (sender, e) =>
+            {
+                int parentWidth = panel_output.ClientSize.Width;
+                int parentHeight = panel_output.ClientSize.Height;
+                int panelSize = Math.Min((int)(parentWidth / _maze.GetLength(1)), (int)(parentHeight / _maze.GetLength(0)));
+
+                // set panel location and size
+                panel.Location = new Point(j * panelSize, i * panelSize);
+                panel.Size = new Size(panelSize, panelSize);
+            };
+
+            // calculate panel size based on the number of rows and columns in _maze
+            int parentWidth = panel_output.ClientSize.Width;
+            int parentHeight = panel_output.ClientSize.Height;
+            int panelSize = Math.Min((int)(parentWidth / _maze.GetLength(1)), (int)(parentHeight / _maze.GetLength(0)));
+
+            // set panel location and size
+            panel.Location = new Point(j * panelSize, i * panelSize);
+            panel.Size = new Size(panelSize, panelSize);
+
+            // check Coloring
+            panel.BackColor = Color.White;
+            if (type == 'X')
+            {
+                panel.BackColor = Color.Black;
+            }
+            else if (type == 'T')
+            {
+                panel.BackColor = Color.Gold;
+            } else if (type == 'K')
+            {
+                krustyKrab = new Point(i, j);
+                panel.BackColor = Color.Red;
+            }
+
+            // create label to display type
+            Label label = new Label();
+            label.Text = type.ToString();
+            label.AutoSize = true;
+            label.Location = new Point(5, 5);
+            label.BackColor = Color.Transparent;
+
+            // add label to panel
+            panel.Controls.Add(label);
+
+            // panel Border
+            panel.BorderStyle = BorderStyle.FixedSingle;
+
+            // add panel to _panels
+            _panels[i, j] = panel;
+
+            // add panel to panel_output
+            panel_output.Controls.Add(_panels[i, j]);
+        }
+
+        private async void updateColorFromRoute(char[] routes)
+        {
+            await Task.Delay(1000); // wait for one second
+            Point toChange = krustyKrab;
+
+            // change the krusty krab color
+            _panels[toChange.Y, toChange.X].BackColor = Color.Green;
+
+            foreach (char route in routes)
+            {
+                await Task.Delay(1000); // wait for one second
+                movePoint(ref toChange, route);
+                _panels[toChange.Y, toChange.X].BackColor = Color.Green;
+            }
+        }
+
+        private void movePoint(ref Point point, char direction)
+        {
+            switch (direction)
+            {
+                case 'L':
+                    point.X -= 1; // move left
+                    break;
+                case 'R':
+                    point.X += 1; // move right
+                    break;
+                case 'U':
+                    point.Y -= 1; // move up
+                    break;
+                case 'D':
+                    point.Y += 1; // move down
+                    break;
+                default:
+                    // invalid direction, do nothing
+                    break;
+            }
+        }
+
+        private void handleInputPanel()
+        {
+            // Create the panel and set its properties
+            Panel myPanel = new Panel();
+            myPanel.BackColor = System.Drawing.Color.LightBlue;
+            myPanel.BorderStyle = BorderStyle.FixedSingle;
+            myPanel.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right | AnchorStyles.Bottom;
+            myPanel.Dock = DockStyle.Fill;
+
+            // Add the panel to the form
+            panel_input.Controls.Add(myPanel);
+
+        }
+
+        
+
+
+        private void handleOutputPanel()
+        {
+        }
+
+        private void handleTitlePanel()
+        {
+            // Create title Label
+
+        }
+
+    }
+}
