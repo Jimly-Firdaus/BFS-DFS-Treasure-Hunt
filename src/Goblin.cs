7a6ef64eb713697f108ee7f8bdc8b26dfeed6177
Jimly-Firdaus <13521102@std.stei.itb.ac.id>
2023-03-19 13:07:56 +0700

    feat: Goblin class

---
+++ src/Goblin.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Goblin
+{
+    public class Goblin : SolveMaze
+    {
+        // private string name;
+        private List<char> route;
+        
+        public Goblin(int totalTreasure, char[,] maze) : base(totalTreasure, maze)
+        {
+            Console.WriteLine("Successfully summon a goblin!");
+        }
+
+        public void SolveWithBFS()
+        {
+            this.route = base.BreadthFirstSearch();
+        }
+
+        public List<char> GetRoute()
+        {
+            return this.route;
+        }
+
+        public int TotalVisitedBFSNodes()
+        {
+            return base.GetTotalBFSNodes();
+        }
+
+    }
+}
