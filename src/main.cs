e1904c24a126d3491212c8c153fd9e0a42b7d1fb
Jimly-Firdaus <13521102@std.stei.itb.ac.id>
2023-03-19 13:09:06 +0700

    feat: main entry point for testing

---
+++ src/main.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Goblin
+{
+    public static class Testing
+    {
+        static void Main(string[] args)
+        {
+            char[,] maze = new char[5, 5]
+            {
+                {'R', 'X', 'R', 'R', 'T'},
+                {'X', 'K', 'R', 'X', 'R'},
+                {'R', 'R', 'T', 'X', 'X'},
+                {'X', 'R', 'X', 'R', 'R'},
+                {'R', 'X', 'R', 'X', 'R'}
+            };
+            Goblin goblin = new Goblin(2, maze);
+            goblin.SolveWithBFS();
+            List<char> route = goblin.GetRoute();
+            Console.WriteLine("Total nodes: " + goblin.TotalVisitedBFSNodes());
+            Console.Write("Route : ");
+            for (int i = 0; i < route.Count; i++)
+            {
+                Console.Write(route[i] + " ");
+            }
+            Console.WriteLine("\nTotal Direction : " + route.Count);
+        }
+    }
+}
